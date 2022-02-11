b43aa89cb1a0583f7b5407e25c223ced82b1bc73
andreashsy <songyanghan+git@gmail.com>
2022-02-11 09:35:01 +0800

    ADD: generated new client components and service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,15 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RecipeListComponent } from './components/recipelist.component';
+import { RecipeDetailComponent } from './components/recipedetail.component';
+import { RecipeAddComponent } from './components/recipeadd.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RecipeService } from './services/recipe.service';
+
+const appRoutes: Routes = [
+  {path: '', component: RecipeListComponent},
+  {path: 'add', component: RecipeDetailComponent},
+  {path: 'todo/:id', component: RecipeAddComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeAddComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    ReactiveFormsModule, FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [RecipeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ client/src/app/services/recipe.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class RecipeService {
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getAllRecipes() {
+
+  }
+}
